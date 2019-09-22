dae2f689b7d55b1d25e5e9fc5188a06f1a58bc41
vivalatrend <41474004+vivalatrend@users.noreply.github.com>
2019-09-22 11:19:09 +0200

    Update install.js

--- install.js
+++ install.js
@@ -2,8 +2,8 @@ const cp = require('child_process');
 
 
 // Setup "youtubeuploader".
-function setup() {
-  console.log('extra: Using setup-youtubeuploader');
-  cp.execSync('npm install -g setup-youtubeuploader', {stdio: [0, 1, 2]});
-};
-setup();
+//function setup() {
+  //console.log('extra: Using setup-youtubeuploader');
+  //cp.execSync('npm install -g setup-youtubeuploader', {stdio: [0, 1, 2]});
+//};
+//setup();
